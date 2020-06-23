946fe96e2d39aedda14af922d565474fb3551124
carminelentisco <carminelentisco@gmail.com>
2020-06-23 16:54:09 +0200

    Modifiche index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,6 +8,13 @@
             <h3 class="author">Autore: {{ $post->user->name }}</h3>
             <h4>Created: {{ $post->created_at }}, Last update: {{ $post->updated_at }}</h4>
             <p>{{ $post->body }}</p>
+            <hr>
+            <h3>Comments:</h3>
+            @foreach($post->comments as $comment)
+                <h4>{{ $comment->name }}</h4>
+                <p>{{ $comment->body }}</p>
+            @endforeach
+            
         </article>
 
         @if (!$loop->last)
